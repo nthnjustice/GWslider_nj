7af05fbc3baea5e1836814518b63d3039ad1ec31
piper-morris <pipermorris5603@gmail.com>
2025-07-07 15:30:35 -0600

    'cleaning things up'

--- GW_class.py
+++ GW_class.py
@@ -39,21 +39,6 @@ class GWSignals:
 
 
 # load data 
-with open('data/GW190521_data_dict.pkl', 'rb') as f:
-    GW150914_data = pickle.load(f)
-
-with open('data/GW190521_data_dict.pkl', 'rb') as f:
-    GW190521_data= pickle.load(f)
-
-with open('data/GW200129_data_dict.pkl', 'rb') as f:
-    GW200129_data= pickle.load(f)
-
-# class instantiation for real GW events
-GW200129 = GWSignals(signal_ref_params['GW200129'][1], GW200129_data)
-GW150914 = GWSignals(signal_ref_params['GW150914'][1], GW150914_data)
-GW190521= GWSignals(signal_ref_params['GW190521'][1], GW190521_data)
-#GW200224= GWSignals(times_200224, strains_200224, strainsBP_200224, signal_ref_params['GW200224'][1], freqs_for_PSD_200224, PSD_200224)
-#GW200311= GWSignals(times_200311, strains_200311, strainsBP_200311, signal_ref_params['GW200311'][1], freqs_for_PSD_200311, PSD_200311)
 with open('data/GW150914_data_dict.pkl', 'rb') as f:
     GW150914_data = pickle.load(f)
 
--- get_data.py
+++ get_data.py
@@ -120,14 +120,6 @@ def get_strain_whitenbp_data(fn_H1, fn_L1, fband, large_data_filename, time_cent
 # with open('data/GW190521_data_dict.pkl', 'wb') as f:
 #      pickle.dump(GW190521_data_dict, f)
 
-#save total data for GW2002129
-
-GW200129_data_dict= get_strain_whitenbp_data('H-H1_GWOSC_4KHZ_R1-1264316101-32.hdf5',
-                                             'L-L1_GWOSC_4KHZ_R1-1264316101-32.hdf5',
-                                             [35., 350.],
-                                             'GWOSC_4KHZ_R1-1264314069-4096.hdf5',
-                                             1264316116.4)
-with open('data/GW200129_data-dict.pkl', 'wb') as f: pickle.dump(GW200129_data_dict, f)
 # # save total data for GW2002129
 
 # GW200129_data_dict= get_strain_whitenbp_data('H-H1_GWOSC_4KHZ_R1-1264316101-32.hdf5',
